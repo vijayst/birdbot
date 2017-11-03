75fb691745d03697a298139b6f0aa50ee867fd43
vijayst <vijayst@gmail.com>
2017-11-03 14:17:43 +0530

    replies to messenger

--- bot.js
+++ bot.js
@@ -28,12 +28,11 @@ class Bot {
                     const { text, nlp } = message.message;
                     if (nlp) {
                         const { entities } = nlp;
-                        console.log(entities.greetings, entities.thanks, entities.bye, this.handleGreeting);
-                        if (entities.bye && entities.bye.value) {
+                        if (entities.bye && entities.bye[0].value) {
                             this.handleBye(senderId);
                         } else if (entities.greetings && entities.greetings[0].value) {
                             this.handleGreeting(senderId);
-                        } else if (entities.thanks && entities.thanks.value) {
+                        } else if (entities.thanks && entities.thanks[0].value) {
                             this.handleThanks(senderId);
                         }
                     }
@@ -45,23 +44,24 @@ class Bot {
     }
 
     handleGreeting(senderId) {
-        console.log('handle greeting');
-        this.sendText(senderId, 'I am a bot. I recognize bird pictures. And tell you the name of the bird.')
-        .catch(err => console.log(err));
+        this.sendText(senderId, 'I am a bot. I recognize bird pictures. And help you with the name of the bird.')
+            .catch(err => console.log(err));
     }
 
     handleBye(senderId) {
         this.sendText(senderId, 'Bye')
+            .catch(err => console.log(err));
     }
 
     handleThanks(senderId) {
-        this.sendText(senderId, 'You are welcome!');
+        this.sendText(senderId, 'You are welcome!')
+            .catch(err => console.log(err));
     }
 
     sendText(id, text) {
         return new Promise((resolve, reject) => {
             request({
-                uri: 'https://graph.facebook.com/2.6/me/messages',
+                uri: 'https://graph.facebook.com/v2.6/me/messages',
                 qs: {
                     access_token: 'EAACLmeHZAgT4BALSgsjv6ljZCPDtotZC08gPn6o9jEIvhyWHiN2FRZBGFtrWISnu1W7ZATnJnOXravhJEhRA3B3flHFJaAGXiBRUow1cZBq0mnHLdWZBInTiWwqDVRyqDRBZB36mkSzOx1HwcRhIfibT67Tsi46x8vI1pufgPH6QYAZDZD'
                 },
@@ -73,7 +73,7 @@ class Bot {
                     message: {
                         text
                     }
-                }  
+                }
             }, (error, response) => {
                 if (!error && response.statusCode === 200) {
                     resolve();
